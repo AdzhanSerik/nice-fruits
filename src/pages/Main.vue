f81b988cee7dfff8a61a2f8db34b5951fc41bc37
AdzhanSerik <adzhan.serik@gmail.com>
2025-04-16 19:29:13 +0500

    first commit

---
+++ src/pages/Main.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-white rounded-[20px] h-screen">
+        <Header />
+    </div>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue'
+</script>
