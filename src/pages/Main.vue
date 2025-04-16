3644a923f812baacf108ae84b62697db66b67e74
AdzhanSerik <adzhan.serik@gmail.com>
2025-04-16 20:02:10 +0500

    dasd

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="bg-white rounded-[20px] h-screen">
         <Header />
+        <Slider />
     </div>
 </template>
 
 <script setup>
 import Header from '../components/Header.vue'
+import Slider from '../components/Slider.vue'
 </script>
