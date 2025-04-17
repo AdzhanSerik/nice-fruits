c84fc40eb41fb4f8a30f01e388784d34cf8aed9c
AdzhanSerik <adzhan.serik@gmail.com>
2025-04-17 19:36:57 +0500

    dasdasd

--- src/components/AllProducts.vue
+++ src/components/AllProducts.vue
@@ -1,19 +1,15 @@
 <template>
     <div class="flex items-center justify-between gap-[40px] flex-wrap mt-10">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product v-for="fruit in fruits" :key="fruit.id" :fruit="fruit" />
     </div>
 </template>
 
 <script setup>
 import Product from './Product.vue'
+import fruits from '../Data/fruits.json'
+
+console.log(fruits)
+
+
+
 </script>
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -4,12 +4,12 @@
         <div class="absolute top-4 left-4 border border-[#D3D3D3] p-2 rounded-[10px]">
             <img src="/favorite.svg" alt="">
         </div>
-        <img width="120px" height="120px" src="/banana.png" alt="">
-        <h3 class="text-[14px] ">Банан</h3>
+        <img width="120px" height="120px" :src="fruit.image" alt="">
+        <h3 class="text-[14px] ">{{ fruit.title }}</h3>
         <div class="flex items-center justify-between w-full">
             <div class="flex flex-col gap-1">
                 <p class="text-[#BDBDBD] text-[11px]">Цена</p>
-                <span class="text-[14px] font-bold">500 тг/кг.</span>
+                <span class="text-[14px] font-bold">{{ fruit.price }} тг/кг.</span>
             </div>
             <div class="border border-[#D3D3D3] p-2 rounded-[10px] border-[2px]">
                 <img src="/add.svg" alt="">
@@ -19,5 +19,7 @@
 </template>
 
 <script setup>
-
+defineProps({
+    fruit: Object
+})
 </script>
